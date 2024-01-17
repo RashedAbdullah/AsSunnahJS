087d459b9ff1d522bc28abac910614c500b3bf9c
rashedabdullah <maarashed40@gmail.com>
2024-01-17 11:43:09 +0600

    more practices in vanilla javascript

--- jsonFile/script.js
+++ jsonFile/script.js
@@ -1,65 +1,61 @@
-
 // Fetch data:
-async function asyncFunc(){
-    try {
-        const url = 'https://jsonplaceholder.typicode.com/photos';
-        const req = await fetch(url);
-        const res = await req.json();
-        res.slice(0, 50).map(value => infoFunc(value))
-    } catch(err){
-        console.log(err.message);
-    }
+async function asyncFunc() {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    const req = await fetch(url);
+    const res = await req.json();
+    res.slice(0, 50).map((value) => infoFunc(value));
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 // Close details pannel:
-function closeMenu(value){
-    value.parentElement.parentElement.parentElement.style.display = "none"
+function closeMenu(value) {
+  value.parentElement.parentElement.parentElement.style.display = "none";
 }
 
 // show details in new pannel:
-const router = (details)=>{
-    const {albumId, id, thumbnailUrl, title, url} = details;
+const router = (details) => {
+  const { albumId, id, thumbnailUrl, title, url } = details;
 
-    const showDetails = document.querySelector(".showDetails");
-    const div = document.createElement('div');
-    showDetails.appendChild(div);
-    div.classList.add('detailsDiv');
+  const showDetails = document.querySelector(".showDetails");
+  const div = document.createElement("div");
+  showDetails.appendChild(div);
+  div.classList.add("detailsDiv");
 
-    div.innerHTML = `<div class="hide">
+  div.innerHTML = `<div class="hide">
         <div>      
-            <p onclick="closeMenu(this)"><i class="fa-solid fa-xmark"></i></p>
+            <div ><button style="cursor: pointer;" onclick="closeMenu(this)"><i class="fa-solid fa-xmark"></i></button></div>
             <img src="${thumbnailUrl}">
             <h2>ID No. ${id}</h2>
             <h3>TITLE: ${title}</h3>
             <h3>URL: ${url}</h3>
         </div>
-    </div>`
-
-}
+    </div>`;
+};
 
 // catching every elements with id:
-async function details(idNo){
-    const fetchFile = await fetch(`https://jsonplaceholder.typicode.com/photos/${idNo}`);
-    const toJson = await fetchFile.json();
-    router(toJson)
-
+async function details(idNo) {
+  const fetchFile = await fetch(
+    `https://jsonplaceholder.typicode.com/photos/${idNo}`
+  );
+  const toJson = await fetchFile.json();
+  router(toJson);
 }
 
 // Show fetch elements:
-function infoFunc(value){
-    const {id, title, Profession} = value; //Destructaring
-    const divElem = document.querySelector(".outerElem");
-            const div = document.createElement("div");
-            divElem.appendChild(div);
-            div.innerHTML = 
-            `<div>
+function infoFunc(value) {
+  const { id, title, Profession } = value; //Destructaring
+  const divElem = document.querySelector(".outerElem");
+  const div = document.createElement("div");
+  divElem.appendChild(div);
+  div.innerHTML = `<div>
                 <img src="${value.thumbnailUrl}" alt"profile">
                 <h3>ID No. ${id}</h3>
                 <p>Title: ${title}</p>
                 <button class="fullInfoBtn" onclick="details(${id})">Full info</button>
-            </div>`
-
-            
+            </div>`;
 }
 
 asyncFunc();
